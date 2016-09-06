5c81cbd4e62607183cec0ccc038e86c1675373ed
shobhitsrivastava <shobhit@gatech.edu>
2016-09-06 16:02:09 -0400

    Make replaceText plugin work, and fix mistakes in injected.js
    added content scripts to manifest.js and jquery code from plugin into
    injected.js. However, the replaceText functionality only works on
    certain pages.

--- injected.js
+++ injected.js
@@ -1,2 +1,3 @@
 document.title = "HARAMBE WAS HERE";
-$("body *").replaceText( /God|Jesus/gi, "Harambe" );
+$("body *").replaceText( /God|Jesus/gi, "HARAMBE" );
+$("body :not(textarea)").replaceText( /God|Jesus/gi, "HARAMBE" );

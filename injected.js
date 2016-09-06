5e994ff17297685f9fa134bf7afcd048536bfb9d
shobhitsrivastava <shobhit@gatech.edu>
2016-09-06 17:06:48 -0400

    Added more dieties
    in injected.js

--- injected.js
+++ injected.js
@@ -1,3 +1,3 @@
 document.title = "HARAMBE WAS HERE";
 $("body *").replaceText( /God|Jesus/gi, "HARAMBE" );
-$("body :not(textarea)").replaceText( /God|Jesus/gi, "HARAMBE" );
+$("body :not(textarea)").replaceText( /God|Jesus|Yahweh|Allah|Brahman/gi, "HARAMBE" );
